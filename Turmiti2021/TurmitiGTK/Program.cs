f8d4de6b7331a50c5d5eeaafe2640abe53c8bea5
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2021-01-23 17:33:15 +0100

    First try in GTK# porting

---
+++ Turmiti2021/TurmitiGTK/Program.cs
@@ -0,0 +1,84 @@
+﻿// copyright (c) 1989,2021 Roberto Ceccarelli - Casasoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of Casasoft Turmiti
+// https://github.com/strawberryfield/Tur-miti
+// 
+// Casasoft Turmiti is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Casasoft Turmiti is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU AGPL v.3
+// along with Casasoft Turmiti.  
+// If not, see <http://www.gnu.org/licenses/>.
+
+using Casasoft.Turmiti.GTK;
+using Gtk;
+using Mono.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+string ExeName = Path.GetFileNameWithoutExtension(AppDomain.CurrentDomain.FriendlyName);
+bool ShouldShowHelp = false;
+bool OnSphere = false;
+List<string> FilesList;
+Application.Init();
+
+OptionSet Options = new OptionSet
+    {
+        { "sphere", "map the world over a sphere (default torus)", o => OnSphere = o != null },
+        { "help", "prints this message and exit", o =>  ShouldShowHelp = o != null },
+    };
+
+try
+{
+    FilesList = Options.Parse(args);
+}
+catch (OptionException e)
+{
+    ShowMessage($"{ExeName}: {e.Message}\nTry '{ExeName} --help' for more informations.");
+    return 1;
+}
+
+if (FilesList == null || FilesList.Count == 0)
+{
+
+    ShowMessage("No input file!");
+    return 1;
+}
+
+if (ShouldShowHelp)
+{
+    Console.WriteLine("Casasoft Tur-miti/GTK#");
+    Console.WriteLine("Copyright 1989,2021 Roberto Ceccarelli - Casasoft\n");
+    Console.WriteLine($"Usage: {ExeName} [options] tablename");
+    Console.WriteLine("\nOptions:");
+    Options.WriteOptionDescriptions(Console.Out);
+    return 0;
+}
+
+Window myWin = new TurmitiForm("Turmiti/GTK#", FilesList[0], OnSphere);
+myWin.Destroyed += delegate
+{
+    Application.Quit();
+};
+myWin.ShowAll();
+
+Application.Run();
+return 0;
+
+void ShowMessage(string message)
+{
+    Dialog dialog = new MessageDialog(null, DialogFlags.Modal, MessageType.Error, ButtonsType.Ok, message);
+    dialog.Title = ExeName;
+    dialog.Run();
+    dialog.Dispose();
+}
---
+++ Turmiti2021/TurmitiGTK/TurmitiForm.cs
@@ -0,0 +1,122 @@
+﻿// copyright (c) 1989,2021 Roberto Ceccarelli - Casasoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of Casasoft Turmiti
+// https://github.com/strawberryfield/Tur-miti
+// 
+// Casasoft Turmiti is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Casasoft Turmiti is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU AGPL v.3
+// along with Casasoft Turmiti.  
+// If not, see <http://www.gnu.org/licenses/>.
+
+using Cairo;
+using Casasoft.Turmiti.Engine;
+using Gtk;
+using System;
+using System.Configuration;
+
+namespace Casasoft.Turmiti.GTK
+{
+    public class TurmitiForm : Window
+    {
+        private Machine machine;
+        private ImageSurface s;
+        private Image img;
+        private string saveName;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+
+        private Color[] colorTable = { new Color(1, 1, 1), new Color(1, 0, 0), new Color(0, 0, 1), new Color(0, 0, 0) };
+
+        public TurmitiForm(IntPtr raw) : base(raw)
+        {
+            Init();
+        }
+
+        public TurmitiForm(WindowType type) : base(type)
+        {
+            Init();
+        }
+
+        public TurmitiForm(string title) : base(title)
+        {
+            Init();
+        }
+
+        public TurmitiForm(string title, string filename, bool isSphere) : base(title)
+        {
+            Init(filename, isSphere);
+        }
+
+        private void Init(string filename, bool isSphere)
+        {
+            machine = new(filename, isSphere);
+            saveName = System.IO.Path.Combine(ConfigurationManager.AppSettings["SavePath"], "Turmiti_");
+
+            Resize(machine.MaxX, machine.MaxY);
+            s = new(Format.RGB24, machine.MaxX, machine.MaxY);
+            img = new(s);
+            Context cr = new(s);
+            cr.SetSourceColor(colorTable[0]);
+            cr.Rectangle(0, 0, machine.MaxX, machine.MaxY);
+            cr.Fill();
+            Add(img);
+
+            KeyPressEvent += TurmitiForm_KeyPressEvent;
+
+            backgroundWorker = new();
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(backgroundWorker_DoWork);
+
+        }
+
+        [GLib.ConnectBefore]
+        private void TurmitiForm_KeyPressEvent(object o, KeyPressEventArgs args)
+        {
+            switch (args.Event.Key)
+            {
+                case Gdk.Key.space:
+                    if (backgroundWorker.IsBusy)
+                    {
+                        backgroundWorker.CancelAsync();
+                    }
+                    else
+                    {
+                        backgroundWorker.RunWorkerAsync();
+                    }
+                    break;
+
+                case Gdk.Key.S:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void Init() => Init(string.Empty, false);
+
+        protected void backgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (!backgroundWorker.CancellationPending)
+            {
+                machine.Next();
+                Context cr = new(s);
+                cr.SetSourceColor(colorTable[machine.CurrentColor]);
+                cr.Rectangle(machine.currentX, machine.currentY, 1, 1);
+                cr.Fill();
+                img.QueueDraw();
+            }
+        }
+
+    }
+}
